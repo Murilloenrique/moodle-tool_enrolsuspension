5899e8a5a88979aaaaeaec1bb5d4a996b417171d
MurilloEnrique <murillojaqueti800@gmail.com>
2026-08-17 20:21:45 -0300

    Corrige PHPDoc dos arquivos de idioma

--- lang/en/tool_enrolsuspension.php
+++ lang/en/tool_enrolsuspension.php
@@ -1,5 +1,5 @@
 <?php
-// This file is part of Moodle - http://moodle.org/
+// This file is part of Moodle - https://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -8,13 +8,20 @@
 //
 // Moodle is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+// along with Moodle. If not, see <https://www.gnu.org/licenses/>.
 
-/** English language strings. @package tool_enrolsuspension */
+/**
+ * English language strings for the enrolment suspension tool.
+ *
+ * @package    tool_enrolsuspension
+ * @copyright  2026 Murillo Enrique
+ * @license    https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
 defined('MOODLE_INTERNAL') || die();
 
 $string['activesuspensions'] = 'Active suspensions';
